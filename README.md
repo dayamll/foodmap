78df83eeb918557393ec8d269f9689b3b42c09c6
dayamll <dayamllerenacastro@gmail.com>
2018-01-01 16:45:22 -0500

    Subiendo modales y funciones

---
+++ README.md
@@ -0,0 +1,19 @@
+# FOODMAP #
+_________________
+
+Flujo de la Aplicacion:
+
+* Vista splash con duración de 2 a 5 segundos que redirecciona a tu vista principal.
+
+* En la vista principal se muestran todos los restaurantes "cerca de ti" junto con el input para filtrar tu elección (Los criterios de filtrado son decisión tuya, puede ser por tipo de comida, costos,orden alfabético, etc).
+
+* Ya que se hizo la elección del filtro deben mostrarse únicamente aquellos restaurantes que cumplan con la condición. 
+
+* Agrega un evento mouseover a las imágenes, lo que pase con el efecto es decisión tuya (Cambio de tamaño, muestra de texto, cambio de imagen, información, etc). 
+
+* Al seleccionar alguno de los restaurantes, deberá mostrarse la información de este a través de un modal. 
+
+* Una vez cerrado el modal debe volver a la vista principal. 
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
 // efecto preloader
 $('#status').delay(3000).fadeOut();
 $('#preloader').delay(3000).fadeOut('slow');
+
+// Restaurantes 
+
+var container = $('.row');
+var modal = $('.modal');
+
+// Funcion que muestra los restaurantes
+function restaurant() {
+  for (i = 0; i < restaurants.length; i++) {
+    container.append('<div class="col-xs-5 col-md-2 group all-boxes" data-name="' + restaurants[i].name + '" data-type="' + restaurants[i].type + '" data-address="' + restaurants[i].address + '" data-toggle="modal" data-target="#myModal" ><p class="titles-box">' + restaurants[i].name + '</p><img class="image"  src=' + restaurants[i].image + '></div>');
+  }
+}
+
+restaurant();
+
+// filtro.
+
+// se ejecuta esta funcion cuando el usuario busca por nombre.
+
+$('#inputSearch').keyup(function() {
+  var name = $(this).val().toLowerCase();
+  $('.group').hide();
+  $('.group').each(function() {
+    var search = $(this).text();
+    if (search.indexOf(name) !== -1) {
+      $(this).show();
+    }
+  });
+});
+
+// Informacion dentro de modal
+
+$('.all-boxes').on('click', function() {
+  for (i = 0; i < restaurants.length; i++) {
+    if ($(this).restaurant('name') === restaurants[i].name);
+    $('.logo-rest').attr('src', restaurants[i].logo);
+    $('.ubication').attr('src', restaurants[i].ubication);
+    $('.direction').text(restaurants[i].description);
+    $('.district').text(restaurants[i].address);
+    $('.restaurants-stars').text(restaurants[i].stars);
+    $('.price').text(restaurants[i].money);
+  }
+});
